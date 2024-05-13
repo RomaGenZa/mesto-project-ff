c82f020ff3ea5c7149ba21fb7e89e1d1aaabf54f
RomaGenZa <147235839+RomaGenZa@users.noreply.github.com>
2024-05-13 19:57:01 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -2,41 +2,31 @@
 const template = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
-const cardList = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 function createCard(cardData, deleteCardCallback) {
   const cloneTemplate = template.cloneNode(true);
-  cloneTemplate.querySelector('.card__image').setAttribute('src', cardData.link);
+  cloneTemplate.querySelector('.card__image').src = cardData.link;
+  cloneTemplate.querySelector('.card__image').alt = cardData.name;
   cloneTemplate.querySelector('.card__title').textContent = cardData.name;
-  const cardId = uuidv4();
-  cloneTemplate.querySelector('.places__item').setAttribute('id', cardId);
+  const cardElement = cloneTemplate.querySelector('.places__item');
   const basket = cloneTemplate.querySelector('.card__delete-button');
   basket.addEventListener('click', function() {
-    deleteCardCallback(cardId);
+    deleteCardCallback(cardElement);
   })
   return cloneTemplate;
 }
 
 // @todo: Функция удаления карточки
-function deleteCard (cardId) {
-  const item = cardList.querySelector('#' + cardId);
-  if (item != null) {
-    item.remove();
-  }
+function deleteCard (cardElement) {
+  cardElement.remove();
 }
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(function(item) { 
-  const card = createCard(item, deleteCard);
-  cardList.append(card);
+initialCards.forEach(function(cardData) { 
+  const card = createCard(cardData, deleteCard);
+  cardsContainer.append(card);
 }); 
 
-// скопированно с 
-//https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid
 
-function uuidv4() {
-  return "id" + "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-    (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
-  );
-}
