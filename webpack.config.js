96b3171528fda35fe9abc3b44a4641232b1903a5
Roman <romagilda@ya.ru>
2024-08-07 13:45:48 +0300

    Completed fixing change requests after review.

--- webpack.config.js
+++ webpack.config.js
@@ -34,8 +34,19 @@ module.exports = {
       // добавили правило для обработки файлов
       {
         // регулярное выражение, которое ищет все файлы с такими расширениями
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        test: /\.(png|svg|jpg|gif)$/,
         type: "asset/resource",
+        generator: {
+          filename: "images/[name].[hash][ext]",
+        },
+      },
+      {
+        // регулярное выражение, которое ищет все файлы с такими расширениями
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name].[hash][ext]",
+        },
       },
       // добавьте ещё одно правило:
       {
