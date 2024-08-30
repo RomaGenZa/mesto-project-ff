2136421cbe62deb55bf1036f3fb446f676468b10
Roman <romagilda@yandex.ru>
2024-08-30 18:53:20 +0300

    api (test)

--- src/api.js
+++ src/api.js
@@ -1,29 +1,46 @@
 import { data } from "autoprefixer";
 
-export const PATH = 'https://mesto.nomoreparties.co/v1/wff-cohort-21';
+export const PATH = "https://mesto.nomoreparties.co/v1/wff-cohort-21";
 
-fetch (`${PATH}/cards`, {
-  headers: {
-    authorization: 'ea2f954f-c019-4b51-8f37-b84bd838696f'
-  },
-  method: 'GET'
-})
-.then((res) => {
-  if (res.ok) {
-    return res.json()
-  }
-})
-.then((data) => {
-  // const ArrayCards = [];
-  const newArrayCards = data.map((item) => {
-    return item;
+function getAllCards() {
+  fetch(`${PATH}/cards`, {
+    headers: {
+      authorization: "ea2f954f-c019-4b51-8f37-b84bd838696f",
+    },
+    method: "GET",
   })
-  // console.log(newArrayCards);
-  // for(let i = 0; length.i; i++) {
-  //   return newArrayCards[i];
-  // }
-  console.log(newArrayCards)
-})
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((data) => {
+      console.log(data);
+    });
+}
 
-// console.log(newArrayCards)
+getAllCards();
 
+function createCard() {
+  fetch(`${PATH}/cards`, {
+    headers: {
+      authorization: "ea2f954f-c019-4b51-8f37-b84bd838696f",
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({
+      name: "Циндао",
+      link: "https://cameralabs.org/media/camera/july/22july/48_366d7b4946bfce955b67b9e638a7b6ec.jpg",
+    }),
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+createCard();
